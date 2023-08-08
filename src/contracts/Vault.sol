62f0b580a0d767750d0291eddc9458dfae1d2453
Prakrit-Jain <prakrit440@gmail.com>
2023-08-08 12:19:26 +0530

    feat: Dummy Vault

---
+++ src/contracts/Vault.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.17;
+
+contract Vault {
+    function initialize(
+        address asset,
+        string memory name,
+        string memory symbol,
+        address roleManager,
+        uint256 profitMaxUnlockTime
+    ) external {}
+}
